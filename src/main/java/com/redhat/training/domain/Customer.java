cba9dd515824f5fad0dbebf6150b371c460832cf
Ricardo Jun <jtaniguc@jtaniguc>
2015-04-27 16:01:41 -0300

    INcluded testing for the service layer

--- src/main/java/com/redhat/training/domain/Customer.java
+++ src/main/java/com/redhat/training/domain/Customer.java
@@ -119,4 +119,34 @@ public class Customer implements Serializable {
 		return orders;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	
+	
 }
--- src/main/java/com/redhat/training/service/NoTokenFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.redhat.training.service;
-
-public class NoTokenFoundException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-}
